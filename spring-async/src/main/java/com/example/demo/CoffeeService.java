0f1dab2a83cf4b27fa58681bc7e08f900b3a1450
Eddy.Kim <sieunkr@gmail.com>
2020-04-26 15:39:07 +0900

    no return value

--- spring-async/src/main/java/com/example/demo/CoffeeService.java
+++ spring-async/src/main/java/com/example/demo/CoffeeService.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 @Slf4j
 @Service
@@ -16,13 +15,26 @@ public class CoffeeService {
     public CompletableFuture<Integer> getPriceAsync(String name) {
 
         try {
-            log.info("start getPrice");
+            log.info("start getPrice..." + name);
             Thread.sleep(3000);
 
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.info(e.getMessage());
         }
 
         return new AsyncResult<>(1200).completable();
+    }
+
+    //리턴을 하지 않는 경우
+    @Async
+    public void order(String name) {
+
+        try {
+            log.info("start order..." + name);
+            Thread.sleep(3000);
+
+        } catch (InterruptedException e) {
+            log.info(e.getMessage());
+        }
     }
 }
